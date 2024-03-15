10faf4d644c1c9cb6c7b43545b70899c18651e2c
Naresh Lokhande <104339530+NareshLokhande@users.noreply.github.com>
2024-03-15 12:40:07 +0530

    Create Q1.java

---
+++ JAVA/Assignment_2/Q1.java
@@ -0,0 +1,12 @@
+package com.assignment3;
+
+public class Q1 {
+	public static void main (String[] args) {
+	
+		int num = 10;
+		double num1 = num;
+		
+		System.out.println(num1+ " is the value after widening converion when the num was " +num);
+		
+	}
+}
